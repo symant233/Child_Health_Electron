e989b3264ae9e2e7325920aa542297750bf8202a
symant233 <symant233@gmail.com>
2020-01-17 19:02:07 +0800

    fix process not defined

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+const process = require('process')
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
